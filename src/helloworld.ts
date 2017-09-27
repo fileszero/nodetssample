4da7280cead63aa2d2fe058d40e07e769602b097
fileszero <files.eq.zero@gmail.com>
2017-09-27 17:05:09 +0900

    compile with tsc --watch

--- src/helloworld.ts
+++ src/helloworld.ts
@@ -13,6 +13,7 @@ class Hello {
 const hello: Hello = new Hello("world");
 hello.sayHello();
 
+process.kill(process.pid);  // it's need for VS code debugger
 // code runner(ctrl+alt+n) need ts-node
 // https://github.com/TypeStrong/ts-node
 // $>npm install -g ts-node
