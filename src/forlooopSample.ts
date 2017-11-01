6eab474c690db1bb2646df7e1bf4e82930cf6441
fileszero <files.eq.zero@gmail.com>
2017-11-01 09:48:37 +0900

    add forEach sample

--- src/forlooopSample.ts
+++ src/forlooopSample.ts
@@ -7,3 +7,23 @@ for (const i in list) {
 for (const i of list) {
     console.log("of:" + i); // "4", "5", "6"
 }
+
+list.forEach((val, idx, arr) => {
+    console.log("forEach:" + val); // "4", "5", "6"
+});
+console.log("end forEach loop");
+
+
+class ThisTest {
+    private theMember = "theMember";
+    public forEachTest(): void {
+        list.forEach((val, idx, arr) => {
+            this.theMember += val;
+            console.log(this.theMember + " forEach:" + val); // "4", "5", "6"
+        });
+        console.log(this.theMember + " end forEach loop");
+    }
+}
+
+let this_test = new ThisTest();
+this_test.forEachTest();
