72dc36a26a4da40e126ec4cabd809d20511acd7f
fileszero <files.eq.zero@gmail.com>
2017-11-10 15:55:37 +0900

    testing with ts-jest

--- src/instancingSample.ts
+++ src/instancingSample.ts
@@ -1,23 +1,21 @@
-namespace instancing {
-    class SomeRecord {
-        constructor(init?: Partial<SomeRecord>) {
-            this.field1 = -1;
-            this.field2 = "not assign";
-            Object.assign(this, init);
-        }
-        public field1: number;
-        public field2: string;
+export class SomeRecord {
+    constructor(init?: Partial<SomeRecord>) {
+        this.field1 = -1;
+        this.field2 = "not assign";
+        Object.assign(this, init);
     }
+    public field1: number;
+    public field2: string;
 
-    const somearray: SomeRecord[] = [
-        { field1: 0, field2: "mem1" },
-        { field1: 1, field2: "mem2" },
-        new SomeRecord({ field1: 2 }),
-        new SomeRecord({ field2: "mem3" }),
-        // { field2: "mem4" },  //これはNG
-    ];
-    for (const some of somearray) {
-        console.log(some);
-    }
+}
 
+const somearray: SomeRecord[] = [
+    { field1: 0, field2: "mem1" },
+    { field1: 1, field2: "mem2" },
+    new SomeRecord({ field1: 2 }),
+    new SomeRecord({ field2: "mem3" }),
+    // { field2: "mem4" },  //これはNG
+];
+for (const some of somearray) {
+    console.log(some);
 }
---
+++ test/instancing.test.ts
@@ -0,0 +1,18 @@
+import { SomeRecord } from "../src/instancingSample";
+namespace instancingTest {
+    describe("instancing test", () => {
+        test("instance without new", () => {
+            const some: SomeRecord = { field1: 0, field2: "mem1" };
+            expect(some.field1).toBe(0);
+        });
+        test("instance with new", () => {
+            const some: SomeRecord = new SomeRecord({ field1: 2 });
+            expect(some.field1).toBe(2);
+        });
+        test("instance with new str", () => {
+            const some: SomeRecord = new SomeRecord({ field2: "some" });
+            expect(some.field1).toBe(-1);
+            expect(some.field2).toBe("some");
+        });
+    });
+}
